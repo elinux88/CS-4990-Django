409da2f716213ad73334e356620af79f17513072
Eli Cook <elinux88@gmail.com>
2015-09-16 19:21:53 -0600

    Added views and templates

--- blog/blogapp/views.py
+++ blog/blogapp/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from .models import Post, Comment, Category
+
+class IndexView(generic.ListView):
+    template_name = 'blogapp/index.html'
+    model = Post
+
+    def get_queryset(self):
+        # SELECT * from blogapp_post ORDER BY pub_date DESC LIMIT 5
+        return Post.objects.order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+    model = Post
+
+#   def get_context_data(self):
+#       context = super(PostDetail, self).get_context_data()
+#       context.update({"comment_list": self.get_object().comment_set.all()})
+#       return context
