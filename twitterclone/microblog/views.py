1dce634cba17d5847fb98e7fe65fbbee26d9ea42
Eli Cook <elinux88@gmail.com>
2015-10-15 16:06:51 -0600

    Added basic templates

--- twitterclone/microblog/views.py
+++ twitterclone/microblog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import DetailView, ListView
 
-# Create your views here.
+class PostListView(ListView):
+    model = Post
+
+class ProfileDetailView(DetailView):
+    model = Profile
+
+    def get_queryset(self):
+        return Post.objects.order_by('-pub_date')[:10]
