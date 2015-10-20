677203107073bf44eebd1b29d5293228ec70921d
Eli Cook <elinux88@gmail.com>
2015-10-19 20:25:11 -0600

    Added details to profile and list pagination

--- twitterclone/microblog/urls.py
+++ twitterclone/microblog/urls.py
@@ -3,5 +3,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-#   url(r'^$', login_required(views.IdeaListView.as_view()), name="idealist"),
+    url(r'^$', views.PostListView.as_view(), name="postlist"),
+    url(r'^profile/(?P<pk>[0-9]+)/$', views.ProfileDetailView.as_view(), name="profiledetail"),
+    url(r'^newpost/$', views.CreatePostView.as_view(), name="addpost"),
+    #url(r'^$', login_required(views.PostListView.as_view()), name="idealist"),
 ]
--- twitterclone/microblog/views.py
+++ twitterclone/microblog/views.py
@@ -1,11 +1,31 @@
 from django.shortcuts import render
 from django.views.generic import DetailView, ListView
+from django.views.generic.edit import CreateView
+
+from .models import Profile, Post
 
 class PostListView(ListView):
     model = Post
+    paginate_by = 10
+
+    def get_queryset(self):
+        return Post.objects.order_by('-pub_date')
 
 class ProfileDetailView(DetailView):
     model = Profile
 
     def get_queryset(self):
         return Post.objects.order_by('-pub_date')[:10]
+
+class CreatePostView(CreateView):
+    model = Post
+    fields = ['body']
+
+    def get_success_url(self):
+        return reverse('microblog:postlist')
+
+    def form_valid(self, form):
+        u = form.save(commit=False)
+        u.profile = Profile.objects.filter(user=self.request.user)[0]
+        u.save()
+        return super(CreatePostView, self).form_valid(form)
