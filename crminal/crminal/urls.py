73aeabf994c6f6b99731f5493e59e52c835434e7
Eli Cook <elinux88@gmail.com>
2015-12-02 18:51:06 -0700

    updated urls

--- crminal/crminal/urls.py
+++ crminal/crminal/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^', include('crm.urls', namespace='crm')),
