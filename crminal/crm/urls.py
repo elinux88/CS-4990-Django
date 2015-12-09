674069b34aef4638adf166611528f0c1f6c2b5d1
Eli Cook <elinux88@gmail.com>
2015-12-09 15:27:11 -0500

    Added Search page

--- crminal/crm/urls.py
+++ crminal/crm/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import patterns, url, include
-from .views import DashboardView, CallLogViewSet, CampaignViewSet, CompanyViewSet, ContactViewSet, OpportunityViewSet, ReminderViewSet, StageViewSet
+from .views import DashboardView, SearchResultsView, CallLogViewSet, CampaignViewSet, CompanyViewSet, ContactViewSet, OpportunityViewSet, ReminderViewSet, StageViewSet
 
 urlpatterns = patterns('',
     url(r'^$', DashboardView.as_view(), name="dashboard"),
+    url(r'^search/$', SearchResultsView.as_view(), name="search"),
     url('', include(CallLogViewSet().urls)),
     url('', include(CampaignViewSet().urls)),
     url('', include(CompanyViewSet().urls)),
